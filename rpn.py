6e45305b495f3443fd44b5d8cce7522c9d4fb059
Xavier <xchoud@umich.edu>
2016-10-07 14:57:29 -0400

    Make it more efficient

--- rpn.py
+++ rpn.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 
+import operator
+
+operators = {
+	'+': operator.add,
+	'-': operator.sub,
+	'*': operator.mul,
+	'/': operator.truediv,
+}
+
 def calculate(myarg1):
 	stack = list()
 	for token in myarg1.split():
-		print(token)
-		if token == '+':
-			arg1 = stack.pop()
-			arg2 = stack.pop()
-			result = arg1 + arg2
-			stack.append(result)
-		elif token == '-':
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = arg1 - arg2
-			stack.append(result)
-		else:
+		try:
 			stack.append(int(token))
+		except ValueError:
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			fucntion = operators[token]
+			result = function(arg1, arg2)
+			stack.append(result)
 		print(stack)
 	if len(stack) != 1:
 		raise TypeError
